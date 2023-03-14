06f6990a2fcbde162ef0984affb9812df5f097aa
Enzo Farias <enzofarias030603@gmail.com>
2023-03-14 13:47:18 -0300

    rename import

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Home } from './Pages/Home'
-import "./global-styles/styles.css"
+import "./globalStyles/global.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
